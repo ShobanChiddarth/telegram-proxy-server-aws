0a370ef756391cc730ae2ab54371877164ba5719
Shoban Chiddarth <messageshoban@gmail.com>
2026-06-23 17:59:03 +0530

    added user_data scripts var

--- aws-infra/vars.tf
+++ aws-infra/vars.tf
@@ -17,3 +17,30 @@ variable "proxy_secret" {
   type = string
   sensitive = false
 }
+
+variable "user_data" {
+  type = map(string)
+  description = "EC2 User Data base"
+
+  default = {
+    base = <<-EOF
+        apt-get update
+        apt-get upgrade -y
+EOF
+
+    docker_install = <<-EOF
+        curl https://get.docker.com | bash
+        adduser ubuntu docker
+EOF
+
+    proxy_server = <<-EOF
+        apt install -y awscli
+        mkdir -p /data
+        export PROXY_SECRET=$(aws ssm get-parameter --name "/tproxy/proxy-secret" --query "Parameter.Value" --output text)
+        echo "$PROXY_SECRET" > /data/secret
+        echo "-- -S $PROXY_SECRET" > /data/secret_cmd
+        docker pull telegrammessenger/proxy:latest
+        docker run -d -p 0.0.0.0:443:443 --name=mtproto-proxy --restart=always -v /data:/data telegrammessenger/proxy:latest
+EOF
+  }
+}
