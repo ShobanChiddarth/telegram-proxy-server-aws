394711b5037c39da3a820b5d49c8d53f4aea295f
Shoban Chiddarth <messageshoban@gmail.com>
2026-06-23 16:52:14 +0530

    Associated to internet rt to subnets

--- aws-infra/subnets.tf
+++ aws-infra/subnets.tf
@@ -4,8 +4,18 @@ resource "aws_subnet" "PublicSubnet1" {
   availability_zone = data.aws_availability_zones.AZs.names[0]
 }
 
+resource "aws_route_table_association" "publicSubnet1RTassoc" {
+  subnet_id = aws_subnet.PublicSubnet1.id
+  route_table_id = aws_route_table.to_igw.id
+}
+
 resource "aws_subnet" "PublicSubnet2" {
   vpc_id = aws_vpc.TProxyVPC.id
   cidr_block = "10.0.2.0/24"
   availability_zone = data.aws_availability_zones.AZs.names[1]
+}
+
+resource "aws_route_table_association" "publicSubnet2RTassoc" {
+  subnet_id = aws_subnet.PublicSubnet2.id
+  route_table_id = aws_route_table.to_igw.id
 }
