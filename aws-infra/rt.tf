f1a60b30f1cdf18f86530275a3bb011d75157203
Shoban Chiddarth <messageshoban@gmail.com>
2026-06-30 21:21:47 +0530

    Created rt to NAT instance

--- aws-infra/rt.tf
+++ aws-infra/rt.tf
@@ -10,3 +10,15 @@ resource "aws_route_table" "to_igw" {
     "Name" = "to_igw"
   }
 }
+
+
+resource "aws_route_table" "to_nat_instance" {
+    vpc_id = aws_vpc.NatInstanceDemoVPC.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        network_interface_id = aws_instance.nat_instance.primary_network_interface_id
+    }
+
+    depends_on = [ aws_eip.nat_instance_eip ]
+}
