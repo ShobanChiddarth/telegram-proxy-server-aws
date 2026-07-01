dbd57d991424733bf63d3d6f5d0dc852901e4492
Shoban Chiddarth <messageshoban@gmail.com>
2026-07-01 10:27:25 +0530

    Updated launch template user_data

--- aws-infra/launch_template.tf
+++ aws-infra/launch_template.tf
@@ -9,6 +9,7 @@ locals {
 
         apt-get install -y unzip
         curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+        unzip awscliv2.zip
         ./aws/install
 
         mkdir -p /data
