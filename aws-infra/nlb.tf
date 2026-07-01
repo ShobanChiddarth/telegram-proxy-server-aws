a05807155daec6003eaa5108a601833c60c831f5
Shoban Chiddarth <messageshoban@gmail.com>
2026-07-01 08:57:37 +0530

    Created listener and target group for the NLB

--- aws-infra/nlb.tf
+++ aws-infra/nlb.tf
@@ -12,3 +12,31 @@ resource "aws_lb" "proxy_nlb" {
 
     enable_cross_zone_load_balancing = true
 }
+
+resource "aws_lb_listener" "proxy_nlb_listener" {
+    load_balancer_arn = aws_lb.proxy_nlb.arn
+
+    port = 443
+    protocol = "TCP"
+
+    default_action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.proxy_target_group.arn
+    }
+}
+
+
+resource "aws_lb_target_group" "proxy_target_group" {
+    name = "proxy_target_group"
+    port = 443
+    protocol = "TCP"
+
+    vpc_id = aws_vpc.TProxyVPC.id
+
+    target_type = "instance"
+
+    health_check {
+        protocol = "TCP"
+        port = "traffic-port"
+    }
+}
