a28b687dacea42185ef230607235d238e7789ed5
Shoban Chiddarth <messageshoban@gmail.com>
2026-07-01 08:34:14 +0530

    Created barebones nlb

---
+++ aws-infra/nlb.tf
@@ -0,0 +1,14 @@
+resource "aws_lb" "proxy_nlb" {
+    name = "proxy_nlb"
+    load_balancer_type = "network"
+    internal = false
+
+    subnets = [ 
+        aws_subnet.PublicSubnet1.id,
+        aws_subnet.PublicSubnet2.id
+    ]
+
+    security_groups = [ aws_security_group.nlb_sg.id ]
+
+    enable_cross_zone_load_balancing = true
+}
