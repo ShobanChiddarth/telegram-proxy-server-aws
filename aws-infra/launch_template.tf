d3c704c3c946fa30ea40ad789c49830cdf7790c6
Shoban Chiddarth <messageshoban@gmail.com>
2026-07-01 09:03:33 +0530

    Correct ssh key name in launch template

--- aws-infra/launch_template.tf
+++ aws-infra/launch_template.tf
@@ -1,7 +1,7 @@
 resource "aws_launch_template" "ProxyInstanceLaunchTemplate" {
     image_id = data.aws_ami.ubuntu.id
     instance_type = "t3.micro"
-    key_name = aws_key_pair.proxy_server_key_pair.key_name
+    key_name = aws_key_pair.management_key_pair.key_name
     vpc_security_group_ids = [ aws_security_group.proxy_server_sg.id ]
     user_data = base64encode(join("\n", [var.user_data.base, var.user_data.docker_install, var.user_data.proxy_server])) # will take care of this later
     update_default_version = true
