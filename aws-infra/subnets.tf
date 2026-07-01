57f2db85d7de513d9a0dd532b66885fd1ac70a26
Shoban Chiddarth <messageshoban@gmail.com>
2026-07-01 08:51:38 +0530

    Associated a route table to private subnet 2

--- aws-infra/subnets.tf
+++ aws-infra/subnets.tf
@@ -48,3 +48,8 @@ resource "aws_subnet" "PrivateSubnet2" {
     cidr_block = "10.0.4.0/24"
     availability_zone = data.aws_availability_zones.AZs.names[1]
 }
+
+resource "aws_route_table_association" "priv2RTassoc" {
+    subnet_id = aws_subnet.PrivateSubnet2.id
+    route_table_id = aws_route_table.to_nat_instance.id
+}
