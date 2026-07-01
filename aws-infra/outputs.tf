d76510365e7cdc2fb9035e53872b8a76202f770c
Shoban Chiddarth <messageshoban@gmail.com>
2026-07-01 09:12:25 +0530

    Created outputs.tf

---
+++ aws-infra/outputs.tf
@@ -0,0 +1,15 @@
+output "nlb_dns" {
+    value = aws_lb.proxy_nlb.dns_name
+}
+
+output "port" {
+    value = aws_lb_listener.proxy_nlb_listener.port
+}
+
+output "proxy_secret" {
+    value = aws_ssm_parameter.proxy_secret.value
+}
+
+output "bastion_public_ip" {
+    value = aws_eip.bastion_eip.public_ip
+}
